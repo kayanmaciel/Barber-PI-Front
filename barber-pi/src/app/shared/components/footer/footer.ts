e6f695717f340c5b40f0ee56d7a9fc1ab47048c4
O-Alex-Tav <271934@unifio.edu.br>
2025-10-14 22:11:01 -0300

    Delete barber-pi directory

--- barber-pi/src/app/shared/components/footer/footer.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-footer',
-  standalone: false, 
-  templateUrl: './footer.html',
-  styleUrls: ['./footer.css']
-})
-export class Footer {}
