acb527104cd1bf5842a4e06f3032e637334a4dcc
O-Alex-Tav <271934@unifio.edu.br>
2025-10-21 23:14:43 +0000

    refactor: new ui design to homepage

---
+++ .angular/cache/20.3.6/UniStore-site/vite/deps/@angular_cdk_bidi.js
@@ -0,0 +1,13 @@
+import {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+} from "./chunk-5XWW6HEW.js";
+import "./chunk-YHNH2NMG.js";
+export {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+};
---
+++ .angular/cache/20.3.6/UniStore-site/vite/deps/@angular_cdk_scrolling.js
@@ -0,0 +1,46 @@
+import {
+  CdkFixedSizeVirtualScroll,
+  CdkScrollable,
+  CdkScrollableModule,
+  CdkVirtualForOf,
+  CdkVirtualScrollViewport,
+  CdkVirtualScrollable,
+  CdkVirtualScrollableElement,
+  CdkVirtualScrollableWindow,
+  DEFAULT_RESIZE_TIME,
+  DEFAULT_SCROLL_TIME,
+  FixedSizeVirtualScrollStrategy,
+  ScrollDispatcher,
+  ScrollingModule,
+  VIRTUAL_SCROLLABLE,
+  VIRTUAL_SCROLL_STRATEGY,
+  ViewportRuler,
+  _fixedSizeVirtualScrollStrategyFactory
+} from "./chunk-T4JO63R7.js";
+import "./chunk-5EG33CFQ.js";
+import "./chunk-3UQT66P7.js";
+import {
+  Dir
+} from "./chunk-5XWW6HEW.js";
+import "./chunk-5LYKDRAH.js";
+import "./chunk-YHNH2NMG.js";
+export {
+  CdkFixedSizeVirtualScroll,
+  CdkScrollable,
+  CdkScrollableModule,
+  CdkVirtualForOf,
+  CdkVirtualScrollViewport,
+  CdkVirtualScrollable,
+  CdkVirtualScrollableElement,
+  CdkVirtualScrollableWindow,
+  DEFAULT_RESIZE_TIME,
+  DEFAULT_SCROLL_TIME,
+  FixedSizeVirtualScrollStrategy,
+  ScrollDispatcher,
+  ScrollingModule,
+  VIRTUAL_SCROLLABLE,
+  VIRTUAL_SCROLL_STRATEGY,
+  ViewportRuler,
+  _fixedSizeVirtualScrollStrategyFactory,
+  Dir as ɵɵDir
+};
